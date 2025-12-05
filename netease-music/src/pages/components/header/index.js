25b53c56b6510eb4888bd4aa9dbe71ae7eb13230
wangjie <wangjie@qqq.com>
2025-12-05 17:13:23 +0800

    feat: 头部和尾部内容搭建

--- netease-music/src/pages/components/header/index.js
+++ netease-music/src/pages/components/header/index.js
@@ -1,10 +1,61 @@
 import { NavLink } from "react-router-dom";
+
+import { HeaderWrapper, HeaderLeft, HeaderRight } from "./style.js";
+import { headerLinks } from "./const.js";
+
+import { Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 export default function Header() {
+  // const getSelectItem = (item,index) => {
+  //   if(index < 3) {
+  //     return (
+  //         <NavLink to={item.link} exact>
+  //           {item.title}
+  //           <i className="sprite_01 icon"></i>
+  //           </NavLink>
+  //     )
+  //   }else {
+  //     return (
+  //       <a href={item.link}>{item.title}</a>
+  //     )
+  //   }
+  // }
   return (
-    <div>
-      <NavLink to="/">发现音乐</NavLink>
-      <NavLink to="/my">我的音乐</NavLink>
-      <NavLink to="/friend">关注</NavLink>
-    </div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a href="#/" className="logo sprite_01">
+            网易云音乐
+          </a>
+          <div className="select-list">
+            {headerLinks.map((item, index) => {
+              return (
+                <div key={item.title} className="select-item">
+                  {index < 3 ? (
+                    <NavLink to={item.link} exact>
+                      {item.title}
+                      <i className="sprite_01 icon"></i>
+                    </NavLink>
+                  ) : (
+                    <a href={item.link}>{item.title}</a>
+                  )}
+                  {/* {getSelectItem(item,index)} */}
+                </div>
+              );
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input
+            className="search"
+            placeholder="音乐/视频/电台/用户"
+            prefix={<SearchOutlined />}
+          ></Input>
+          <div className="center">创作者中心</div>
+          <div className="">登录</div>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
   );
 }
--- netease-music/src/router/index.js
+++ netease-music/src/router/index.js
@@ -2,19 +2,25 @@ import Discover from "../pages/discover";
 import Friend from "../pages/friend";
 import MyMusic from "../pages/my-music";
 
-const routes = [{
-        path: '/',
-        exact: true,
-        component: Discover
-    },
-    {
-        path: '/my',
-        component: MyMusic
-    },
-    {
-        path: '/friend',
-        component: Friend
-    },
-]
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    component: Discover,
+  },
+  {
+    path: "/discover",
+    exact: true,
+    component: Discover,
+  },
+  {
+    path: "/my",
+    component: MyMusic,
+  },
+  {
+    path: "/friend",
+    component: Friend,
+  },
+];
 
-export default routes
+export default routes;
